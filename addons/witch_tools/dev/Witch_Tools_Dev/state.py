c1af8aee4ae75e29c5d24fb25e655ee39c3f0f3c
Knight Witch <amanda.m.ivans@gmail.com>
2026-08-08 14:15:34 -0700

    Update Witch Tools v2.10 runtime state metadata

--- addons/witch_tools/dev/Witch_Tools_Dev/state.py
+++ addons/witch_tools/dev/Witch_Tools_Dev/state.py
@@ -1,19 +1,12 @@
 ADDON_PACKAGE = __package__
-ADDON_VERSION = (2, 9, 0)
+ADDON_VERSION = (2, 10, 0)
 IS_DEV_BUILD = True
-VERSION_LABEL = 'Dev_v2.9.0' if IS_DEV_BUILD else 'v2.9.0'
+VERSION_LABEL = 'Dev_v2.10.0' if IS_DEV_BUILD else 'v2.10.0'
 ADDON_NAME = f'Witch Tools {VERSION_LABEL}'
 BUILD_TARGET_LABEL = 'Blender 4.5'
 PANEL_CATEGORY = 'Witch Tools'
 MODE_SWITCHER_SEQUENCE = (
-    'EDIT',
-    'OBJECT',
-    'WEIGHT_PAINT',
-    'POSE',
-    'SCULPT',
-    'TEXTURE_PAINT',
-    'VERTEX_PAINT',
-    'UV_DATA',
+    'EDIT', 'OBJECT', 'WEIGHT_PAINT', 'POSE', 'SCULPT', 'TEXTURE_PAINT', 'VERTEX_PAINT', 'UV_DATA',
 )
 
 MODE_CYCLE_ITEMS = (
@@ -37,54 +30,26 @@ FOOTER_LINKS = (
 )
 
 UI_STATE_PROPS = (
-    'show_modifier_shrinkwrap',
-    'show_modifier_batch_tools',
-    'show_edit_vertex_snap',
-    'show_edit_object_snap',
-    'show_edit_vertex_inject',
-    'show_edit_curvature_sync',
-    'show_edit_guided_align',
-    'show_edit_selection_slots',
-    'show_edit_vertex_locks',
-    'show_vertex_locks_core',
-    'show_vertex_locks_zone',
-    'show_vertex_locks_groups',
-    'show_vertex_locks_sculpt',
-    'show_vertex_locks_shape',
+    'show_modifier_shrinkwrap', 'show_modifier_batch_tools',
+    'show_edit_vertex_snap', 'show_edit_object_snap',
+    'show_edit_vertex_inject', 'show_edit_curvature_sync', 'show_edit_guided_align',
+    'show_edit_edge_doctor', 'show_edge_doctor_inject', 'show_edge_doctor_alignment', 'show_edge_doctor_curvature',
+    'show_edit_selection_slots', 'show_edit_vertex_locks',
+    'show_vertex_locks_core', 'show_vertex_locks_zone', 'show_vertex_locks_groups', 'show_vertex_locks_sculpt', 'show_vertex_locks_shape',
     'vertex_locks_group_filter',
-    'show_weight_transfer',
-    'show_weight_transfer_howto',
-    'show_weight_transfer_single',
-    'show_weight_transfer_batch',
-    'show_weight_mirror',
-    'show_weight_mirror_howto',
-    'show_armature_cleanup',
-    'show_head_prep',
-    'show_head_editing',
-    'show_head_finishing',
-    'show_footer_cycle_settings',
-    'show_footer_hotkeys',
+    'show_weight_transfer', 'show_weight_transfer_howto', 'show_weight_transfer_single', 'show_weight_transfer_batch',
+    'show_weight_mirror', 'show_weight_mirror_howto', 'show_armature_cleanup',
+    'show_head_prep', 'show_head_editing', 'show_head_finishing',
+    'show_footer_cycle_settings', 'show_footer_hotkeys',
 )
 
 PANEL_ORDERS = {
-    'MODE_SWITCHER': 0,
-    'AUTO_MIRROR': 1,
-    'EDIT_TOOLS': 2,
-    'MODIFIER_TOOL': 3,
-    'HEAD_TOOLS': 4,
-    'BODY_TOOLS': 5,
-    'ARMOUR_TOOLS': 6,
-    'HAIR_TOOLS': 7,
-    'WEIGHT_TOOLS': 8,
-    'ARMATURE_TOOLS': 9,
-    'SHAPE_KEY_TOOLS': 10,
-    'EXPORT_TOOLS': 11,
-    'TROUBLESHOOTING_TOOLS': 12,
-    'FOOTER': 99,
+    'MODE_SWITCHER': 0, 'AUTO_MIRROR': 1, 'EDIT_TOOLS': 2, 'MODIFIER_TOOL': 3,
+    'HEAD_TOOLS': 4, 'BODY_TOOLS': 5, 'ARMOUR_TOOLS': 6, 'HAIR_TOOLS': 7,
+    'WEIGHT_TOOLS': 8, 'ARMATURE_TOOLS': 9, 'SHAPE_KEY_TOOLS': 10, 'EXPORT_TOOLS': 11,
+    'TROUBLESHOOTING_TOOLS': 12, 'FOOTER': 99,
 }
 
 addon_keymaps = []
 armature_hide_select_state = {}
-runtime_cache = {
-    'weight_paint_key': None,
-}
+runtime_cache = {'weight_paint_key': None}
