2f96f3d9372b1863e138300079be0057c6f3ad82
Knight Witch <amanda.m.ivans@gmail.com>
2026-08-08 19:25:58 -0700

    Update Dev v2.10.1 state metadata

--- addons/witch_tools/dev/Witch_Tools_Dev/state.py
+++ addons/witch_tools/dev/Witch_Tools_Dev/state.py
@@ -1,7 +1,7 @@
 ADDON_PACKAGE = __package__
-ADDON_VERSION = (2, 10, 0)
+ADDON_VERSION = (2, 10, 1)
 IS_DEV_BUILD = True
-VERSION_LABEL = 'Dev_v2.10.0' if IS_DEV_BUILD else 'v2.10.0'
+VERSION_LABEL = 'Dev_v2.10.1' if IS_DEV_BUILD else 'v2.10.1'
 ADDON_NAME = f'Witch Tools {VERSION_LABEL}'
 BUILD_TARGET_LABEL = 'Blender 4.5'
 PANEL_CATEGORY = 'Witch Tools'
