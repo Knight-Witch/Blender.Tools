55c5acf75a337b93c13f9ccdf38743b159b7b543
Knight Witch <amanda.m.ivans@gmail.com>
2026-08-18 21:09:32 -0700

    Bump Witch Tools to Dev_v2.11.3

--- addons/witch_tools/dev/Witch_Tools_Dev/__init__.py
+++ addons/witch_tools/dev/Witch_Tools_Dev/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
-    'name': 'Witch Tools Dev_v2.11.2',
+    'name': 'Witch Tools Dev_v2.11.3',
     'author': 'Knight Witch',
-    'version': (2, 11, 2),
+    'version': (2, 11, 3),
     'blender': (4, 5, 0),
     'location': 'View3D > Sidebar > Witch Tools',
     'description': 'Workflow-organized Blender tools with integrated Transform, 3D-print analysis/repair/cleanup, Magic Branch, BG3, weight, modifier, and modeling workflows',
